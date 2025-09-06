89e0d4bacd58d8fdd991b1bf068b3eda8798eaa9
sanket0099 <145530164+sanket0099@users.noreply.github.com>
2025-09-06 12:22:24 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,3 +8,5 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
+
+
